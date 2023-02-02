de99abdf58755a8fdb2645fba940976902c3ae57
nandovbr <nandovbr15@hotmail.com>
2023-02-02 10:24:22 -0300

    ajustes para melhor organização do código

---
+++ src/screens/home/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './home.css';
+import Library from '../library/index';
+import Feed from '../feed/index';
+import Trending from '../trending/index';
+import Player from '../player/index';
+import Favorites from '../favorites/index';
+import Sidebar from '../../components/sidebar/index';
+
+export default function Home() {
+  return (
+    <Router>
+      <div className="main-body">
+        <Sidebar />
+
+        <Routes>
+          <Route path="/" element={<Library />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/tendencias" element={<Trending />} />
+          <Route path="/player" element={<Player />} />
+          <Route path="/favoritos" element={<Favorites />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
