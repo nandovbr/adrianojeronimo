a1b5235ec99b6c38e78a99281c1800f1a79532e3
nandovbr <nandovbr15@hotmail.com>
2023-02-15 19:31:28 -0300

    ajustes na estilização

--- src/screens/lancamento/index.jsx
+++ src/screens/lancamento/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import ReactPlayer from 'react-player';
+import './lancamento.css'
 
 export default function Lancamento() {
   return (
-    <div className="screen-container">Lancamento Aqui</div>
+    <div className="screen-container">
+      <ReactPlayer url="https://www.youtube.com/watch?v=MOK8LNRKXzQ" />
+    </div>
   )
 }
--- src/screens/player/index.jsx
+++ src/screens/player/index.jsx
@@ -1,35 +1,8 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import Login from '../auth/login';
-import { setClientToken } from '../../spotify';
+import './player.css'
 
 export default function Player() {
-
-  
-  const [token, setToken] = useState("");
-
-  // token de acesso e salva no localstorage
-  useEffect(() => {
-    const isToken = window.localStorage.getItem('token');
-    const hash = window.location.hash;
-    // console.log(hash.split('&')[0].split('=')[1]);
-
-    window.location.hash = "";
-
-    if (!isToken && !hash) {
-      const token = hash.split('&')[0].split('=')[1];
-      window.localStorage.setItem('token', token);
-      setToken(token);
-      setClientToken(token)
-    } else {
-      setToken(isToken);
-      setClientToken(isToken)
-    }
-  }, []);
-
-  return !token ? (
-    <Login />
-  ) : (
+  return (
     <>
       <div className="screen-container">
         Player Aqui        
