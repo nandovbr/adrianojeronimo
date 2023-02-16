ad049f18b916ff850fbe5f2b07cfb3805ea32497
nandovbr <nandovbr15@hotmail.com>
2023-02-16 10:41:55 -0300

    ajustes de estilização

--- src/screens/player/index.jsx
+++ src/screens/player/index.jsx
@@ -1,16 +1,23 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import './player.css'
 
 export default function Player() {
   return (
-    <>
+    <div className="container">
       <div className="music-container">
-        <h3>Rei da Glória</h3>
-        <audio src={ require("./musicas/musica-Adriano.mp3") } controls />
-
-        <h3>Sementes</h3>
-        <audio src={ require("./musicas/testeaudio.ogg") } controls />
+        <img src="https://i.postimg.cc/wjDdX0xr/Rei-da-Gl-ria.jpg" className="music-image" />
+        <div className="music">
+          <audio src={ require("./musicas/musica-Adriano.mp3") } controls />
+        </div>
       </div>
-    </>
+
+      <div className="music-container">
+      <img src="https://i.postimg.cc/wjDdX0xr/Rei-da-Gl-ria.jpg" className="music-image" />
+        <div className="music">
+          <audio src={ require("./musicas/musica-Adriano.mp3") } controls />
+        </div>
+      </div>
+    </div>
   )
 }
