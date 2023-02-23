829d5e1dbfc03fbbb88ba0654799d977a5cd2791
nandovbr <nandovbr15@hotmail.com>
2023-02-23 08:06:57 -0300

    adiciona as músicas no player

--- src/screens/player/index.jsx
+++ src/screens/player/index.jsx
@@ -4,18 +4,52 @@ import './player.css'
 
 export default function Player() {
   return (
-    <div className="container">
-      <div className="music-container">
-        <img src="https://i.postimg.cc/wjDdX0xr/Rei-da-Gl-ria.jpg" className="music-image" />
-        <div className="music">
-          <audio src={ require("./musicas/musica-Adriano.mp3") } controls controlsList="nodownload" />
+    <div className="principal">
+      <div className="container">
+        <div className="music-container">
+          <img src="https://i.postimg.cc/wjDdX0xr/Rei-da-Gl-ria.jpg" className="music-image" />
+          <div className="music">
+            <audio src={ require("./musicas/reiDaGloria.mp3") } controls controlsList="nodownload" />
+          </div>
+        </div>
+
+        <div className="music-container">
+          <img src="https://i.postimg.cc/wjDdX0xr/Rei-da-Gl-ria.jpg" className="music-image" />
+          <div className="music">
+            <audio src={ require("./musicas/apocalipseMaster02.mp3") } controls controlsList="nodownload" />
+          </div>
         </div>
       </div>
 
-      <div className="music-container">
-      <img src="https://i.postimg.cc/wjDdX0xr/Rei-da-Gl-ria.jpg" className="music-image" />
-        <div className="music">
-          <audio src={ require("./musicas/testeaudio.ogg") } controls controlsList="nodownload" />
+      <div className="container">
+        <div className="music-container">
+          <img src="https://i.postimg.cc/wjDdX0xr/Rei-da-Gl-ria.jpg" className="music-image" />
+          <div className="music">
+            <audio src={ require("./musicas/ontemHojeESempreMaster.mp3") } controls controlsList="nodownload" />
+          </div>
+        </div>
+
+        <div className="music-container">
+          <img src="https://i.postimg.cc/wjDdX0xr/Rei-da-Gl-ria.jpg" className="music-image" />
+          <div className="music">
+            <audio src={ require("./musicas/sementesMaster02.wav") } controls controlsList="nodownload" />
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="music-container">
+          <img src="https://i.postimg.cc/wjDdX0xr/Rei-da-Gl-ria.jpg" className="music-image" />
+          <div className="music">
+            <audio src={ require("./musicas/sementesLadoBMaster02.mp3") } controls controlsList="nodownload" />
+          </div>
+        </div>
+
+        <div className="music-container">
+          <img src="https://i.postimg.cc/wjDdX0xr/Rei-da-Gl-ria.jpg" className="music-image" />
+          <div className="music">
+            <audio src={ require("./musicas/Salmo51Master.mp4") } controls controlsList="nodownload" />
+          </div>
         </div>
       </div>
     </div>
